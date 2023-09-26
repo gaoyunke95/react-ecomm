6fd62c2cc11bc04a53fc04059a6a3eaf983ea51f
Henry Gao <gaoyunke95@gmail.com>
2023-09-26 12:44:07 -0400

    setup navi styles

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,23 @@
-﻿import { Outlet } from "react-router-dom";
+﻿import { Outlet , Link} from "react-router-dom";
+import { Fragment } from "react";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
-      <div>
-        <div>
-          <h1>this is navigation bar</h1>
+      <Fragment>
+        <div className='navigation'>
+          <Link className='logo-container' to='/'>
+            <CrownLogo className="logo"/>
+          </Link>
+          <div className='nav-links-container'>
+            <Link className="nav-link" to='/shop'>
+              SHOP
+            </Link>
+          </div>
         </div>
         <Outlet/>
-      </div>
+      </Fragment>
   
     )
 }
