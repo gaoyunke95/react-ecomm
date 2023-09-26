24fe36cf5903f39cc1ff650f0fd656e206f86a52
Henry Gao <gaoyunke95@gmail.com>
2023-09-26 10:43:07 -0400

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-function App() {
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>this is shop page</h1>
+    </div>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <Directory/>
+      <Routes>
+        <Route path='/' element={<Navigation />} >
+          <Route index element={<Home />}/>
+          <Route path='shop' element={<Shop />}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,14 @@
+﻿import { Outlet } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+      <div>
+        <div>
+          <h1>this is navigation bar</h1>
+        </div>
+        <Outlet/>
+      </div>
+  
+    )
+}
+export default Navigation;
